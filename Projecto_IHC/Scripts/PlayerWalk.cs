cd8a963ddb230a2876564b24fb7cbf781c4a29ea
Fernando Ttito <luis.ttito@ucsp.edu.pe>
2019-03-30 18:13:10 -0500

    v1

---
+++ Projecto_IHC/Scripts/PlayerWalk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerSpeed;
+
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
+        //transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+    }
+}
